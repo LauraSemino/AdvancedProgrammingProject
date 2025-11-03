fa27e39301fd78251a6be9e8867ace817d331ac9
LauraSemino <seminol@sheridancollege.ca>
2025-11-03 14:24:36 -0500

    Gamepad responds in game
    Next is figuring out how to make it know whether your using keyboard or gamepad

--- AdvancedProgrammingProject/Assets/Scripts/PlayerController.cs
+++ AdvancedProgrammingProject/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public int pNum;
     public Rigidbody2D rb;
+    public float speed;
+
+    Vector2 dir;
+
+    public string deviceType;
+    Gamepad currentGamepad;
+    Keyboard currentKeyboard;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +21,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentGamepad = Gamepad.current;
+        if (currentGamepad != null)
+        {
+            Debug.Log(currentGamepad.name);
+            return;
+           //connected device
+        }
+       
+    }
+
+    private void FixedUpdate()
+    {     
+        rb.linearVelocityX = dir.x * speed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        dir = currentGamepad.leftStick.ReadValue();
     }
 }
